f36bda819d2a757b3bd3319f5e014426803bcae7
7moodev <67708016+7moodev@users.noreply.github.com>
2025-01-09 23:21:29 +0100

    first commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,9 @@
+import { ActionGetResponse } from "@solana/actions"
+
+
+export async function GET(request: Request) {
+  // const Response:ActionGetResponse = {
+  //   icon:
+  // }
+  return new Response('Hello, from API!')
+}
